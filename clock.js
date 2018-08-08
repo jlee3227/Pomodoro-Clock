0a337223d7f5a9419f95d39d073b9ef7adbf79cb
Joon Lee <joon.lee727@gmail.com>
2018-08-08 14:24:01 -0400

    Add comments

--- clock.js
+++ clock.js
@@ -10,17 +10,16 @@ const increment = document.querySelectorAll(".increase");
 const decrement = document.querySelectorAll(".decrease");
 
 
-var startTime = new Date();     //
-var remainTime = new Date();    //
+var startTime = new Date();     //Global variable for a starting time
 var isOn = false;               //A sentinel value to see if the timer is running
 var active = false;             //Sentinel value for checking if the timer has been activated
 
-var sessionTracker = 0;
-var titleArr = ["Work", "Break"];
+var sessionTracker = 0;             //Variable used to keep track of which session is currently active, 0 is for work, 1 is for break
+var titleArr = ["Work", "Break"];   //An array containing the names of the sessions
 
-var timeInterval;               //Using this global variable (temporarily?) to stop and start the timer
+var timeInterval;               //Global variable to stop and start the timer
 
-
+//Adding event listeners to each of the controls
 controls.forEach((ctrl) => {
     ctrl.addEventListener('click', (e) => {
         if(ctrl.id == "start" && !isOn){
@@ -37,6 +36,7 @@ controls.forEach((ctrl) => {
             stop();
             console.log("stopped");
         }
+        //A hard reset of the timer
         if(ctrl.id == "reset"){
             workTime.textContent = "25";
             breakTime.textContent = "5"
@@ -52,9 +52,9 @@ increment.forEach((btn) => {
         if(!isOn && !active){
             if(btn.parentNode.className == "work"){
                 workTime.textContent = (parseInt(workTime.textContent) + 1).toString();
+                //If the timer is greater than or equal to 60 minutes then adjust the timer display
                 if(parseInt(workTime.textContent) >= 60)
-                    timer.textContent = ('0' + Math.floor(parseInt(workTime.textContent) / 60)).slice(-2) + 
-                            ":" + ("0" + (parseInt(workTime.textContent) % 60)).slice(-2) + ":00";
+                    timer.textContent = ('0' + Math.floor(parseInt(workTime.textContent) / 60)).slice(-2) + ":" + ("0" + (parseInt(workTime.textContent) % 60)).slice(-2) + ":00";
                 else
                     timer.textContent = ("0" + workTime.textContent).slice(-2) + ":00";
             }
@@ -72,9 +72,9 @@ decrement.forEach((btn) => {
             //To prevent the values from being unreasonably small (less than 1 minute), the value of the sessions must be at least 1
             if(btn.parentNode.className == "work" && parseInt(workTime.textContent) > 1){
                 workTime.textContent = (parseInt(workTime.textContent) - 1).toString();
+                 //If the timer is greater than or equal to 60 minutes then adjust the timer display
                 if(parseInt(workTime.textContent) >= 60)
-                    timer.textContent = ('0' + Math.floor(parseInt(workTime.textContent) / 60)).slice(-2) + 
-                            ":" + ("0" + (parseInt(workTime.textContent) % 60)).slice(-2) + ":00";
+                    timer.textContent = ('0' + Math.floor(parseInt(workTime.textContent) / 60)).slice(-2) + ":" + ("0" + (parseInt(workTime.textContent) % 60)).slice(-2) + ":00";
                 else
                     timer.textContent = ("0" + workTime.textContent).slice(-2) + ":00";
             }
@@ -100,41 +100,49 @@ function countdown(start){
 
 //Used when the pause buttong is pressed to determine how much time is remaining (in milliseconds)
 function timeRemaining(){
-    var strArr = timer.textContent.split(":").map(num => parseInt(num)); //takes the numbers displayed on the timer, converts them to integers then stores them in an array
+    var strArr = timer.textContent.split(":").map(num => parseInt(num)); //Takes the numbers displayed on the timer, converts them to integers then stores them in an array
     var timeLeft = 0;
+    //Converts the integers stored in the array into seconds, minutes and hours
     for(var i=0; i<strArr.length; i++){
         timeLeft += strArr[i] * 1000 * Math.pow(60, (strArr.length - 1 - i));
     }
     return timeLeft;
 }
 
+//The actual timer functionality
 function startTimer(start){
+    //This function gets the time remaining and updates the display
     function update(){
         var time = countdown(start)
         timer.textContent = '';
+        //If the time remaining exceeds 1 hour, adjust the display to include hours
         if(time.total > 3600000)
-            timer.textContent += ('0' + time.hours).slice(-1) + ":";
+            timer.textContent += ('0' + time.hours).slice(-2) + ":";
         timer.textContent += ('0' + time.minutes).slice(-2) + ":";
         timer.textContent += ('0' + time.seconds).slice(-2);
 
+        //If the time remaining is less than or equal to 0, change sessions
         if(time.total <= 0){
-            //clearInterval(timeInterval);
-            sessionTracker = (sessionTracker + 1) % 2;
-            session.textContent = titleArr[sessionTracker];
+            sessionTracker = (sessionTracker + 1) % 2;          //Update the session tracker
+            session.textContent = titleArr[sessionTracker];     //Change the session title accordingly
+            //Change display timer accordingly
             timer.textContent = (sessionTracker == 0) ? (("0" + workTime.textContent).slice(-2) + ":00") : (("0" + breakTime.textContent).slice(-2) + ":00");
-            start = Date.parse(new Date()) + timeRemaining() + 1000;
+            start = Date.parse(new Date()) + timeRemaining() + 1000;    //Update the time remaining, + 1000 to adjust for time lag
         }
     }
 
     update();
-    timeInterval = setInterval(update, 1000);
+    timeInterval = setInterval(update, 1000);   //Call the update function every second
 }
 
+
+//Pauses the timer
 function pause(){
     clearInterval(timeInterval);
     isOn = false;
 }
 
+//Stops the timer and does a soft reset
 function stop(){
     pause();
     active = false;
